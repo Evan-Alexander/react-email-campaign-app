dd48f4e16d88a7f6ac93026a152c6a367b200d32
jb <jasontbrown99@gmail.com>
2018-02-28 11:15:40 -0800

    change header content with uri paths on login/logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 
 class Header extends Component {
   renderContent() {
+    console.log(this.props.auth);
     switch (this.props.auth) {
       case null:
         return;
       case false:
         return <li><a href="/auth/google">Login With Google</a></li>;
       default:
-      return <li><a>Logout</a></li>;
+        return <li><a href="/api/logout">Logout</a></li>;
     }
   }
 
