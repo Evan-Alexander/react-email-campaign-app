0be8cd44c075bcd77c8ec3dd5495c3074300f8b7
jb <jasontbrown99@gmail.com>
2018-03-01 10:54:46 -0800

    add credits to user model; update billingRoutes to send response with updated credits upon purchase; include middleware for added authenticion

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,15 +1,24 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
-
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
+  // we just have to  make a reference to requireLogin for it to work in our billing
+  // post request.
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: '$5 for five credits',
       source: req.body.id
     });
-    console.log(charge);
+    req.user.credits += 5;
+    // even though this looks like we're updating a constant variable ...
+    // when ever we save a user model, we are making a reference of the user model
+    // that we just got back from the database
+    // These are seperate models, even though they represent the same thing
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
