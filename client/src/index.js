347628bc6411f352154709612bd99cb5c7091b72
jb <jasontbrown99@gmail.com>
2018-02-28 21:46:14 -0800

    add stripe and react-stripe-checkout; create payment component; wire payment component to header component

--- client/src/index.js
+++ client/src/index.js
@@ -16,3 +16,5 @@ ReactDom.render(
   </Provider>,
   document.querySelector('#root')
 );
+console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+console.log('Environment is', process.env.NODE_ENV);
