8fb13d6e3884c43e214042889caf1059cfd19250
jb <jasontbrown99@gmail.com>
2018-03-01 09:56:12 -0800

    install packages stripe and body-parser; create billingRoute; returns charge object

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -11,6 +12,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// Intitialize Middleware
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     // 30 days a month / 24hrs a day / 60 min in hr / 60 sec in min / 1000 ms in sec
@@ -23,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
