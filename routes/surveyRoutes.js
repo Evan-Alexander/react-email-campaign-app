81cdabe0ba6ad17860a4189a5636d314c95e1ba9
jb <jasontbrown99@gmail.com>
2018-03-05 12:23:27 -0800

    add sendgrid to project; add routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-const
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      // Take the list of emails, comma seperate them, then return an email with the key of email and value of the email itself.
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
