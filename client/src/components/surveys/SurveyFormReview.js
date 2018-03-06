507f832b603af4e4a09a4c449abd0c7830da3d9d
jb <jasontbrown99@gmail.com>
2018-03-06 13:35:02 -0800

    create send survey btn; wire it up to accept action reducer

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -23,9 +24,15 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       {reviewFields}
     </div>
     <button
-      className="yellow darken-3 btn-flat"
+      className="yellow darken-3 btn-flat white-text"
       onClick={onCancel}>
       Back
+    </button>
+    <button
+      onClick={() => submitSurvey(formValues)}
+      className="green btn-flat right white-text">
+      Send Survey
+      <i className="material-icons right">email</i>
     </button>
     </div>
   );
@@ -36,4 +43,4 @@ function mapStateToProps(state) {
     formValues: state.form.surveyForm.values
   };
 }
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
