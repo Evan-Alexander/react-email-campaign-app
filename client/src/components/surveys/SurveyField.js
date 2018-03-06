56a20eb7c706f51ea84bbf42f69a2f23429d4a5a
jb <jasontbrown99@gmail.com>
2018-03-05 16:16:46 -0800

    create first surveyfield component; begin refactor of surveyform

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logic to render a single label and text input
+import React from 'react';
+
+class export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+}
